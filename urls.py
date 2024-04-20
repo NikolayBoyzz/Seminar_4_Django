716892f04860bc07230c366886729c62523e76d9
NikolayBoyzz <123127020+NikolayBoyzz@users.noreply.github.com>
2024-04-20 22:43:32 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import include, path
+
+from .views import OrderList
+
+from django.urls import include, path
+
+from .views import Indication_price
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about')),
+    path(
+        "indication_price/<int:user_id>/<int:timespan_in_days>/",
+        Indication_price.as_view(),
+        name="indication_price",
+    ),
 ]
--- views.py
+++ views.py
@@ -1,1 +1,30 @@
-from django.shortcuts import render
+from datetime import date, timedelta
+
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+
+from myapp1 import models
+
+
+class IndicationPrice(View):
+    def get(self, request, user_id, timespan_in_days) -> HttpResponse:
+        client = get_object_or_404(models.Client, pk=user_id)
+        timespan = timedelta(days=timespan_in_days)
+        indication_price = models.Indication_price.objects.filter(customer=client).filter(
+            date_ordered__gte=(date.today() - timespan)
+        )
+
+        o = []
+        for indication_price in indication_price:
+            products = indication_price.products.all()
+            o.append({"order_data": indication_price, "products": products})
+
+        context = {
+            "client": client,
+            "orders": o,
+            "period": timespan_in_days,
+        }
+        return render(
+            request=request, template_name="myapp2/indication_price.html", context=context
+        )
